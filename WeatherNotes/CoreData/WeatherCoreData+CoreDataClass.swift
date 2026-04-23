87cbcc3de9067ee195e614c0353ac7e6dc114b5a
uidls31 <lagutin7234@gmail.com>
2026-04-23 15:45:36 +0200

    Added Core Data

---
+++ WeatherNotes/CoreData/WeatherCoreData+CoreDataClass.swift
@@ -0,0 +1,29 @@
+public import Foundation
+public import CoreData
+
+public typealias WeatherCoreDataCoreDataClassSet = NSSet
+
+@objc(WeatherCoreData)
+public class WeatherCoreData: NSManagedObject {
+
+}
+
+extension WeatherCoreData {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<WeatherCoreData> {
+        return NSFetchRequest<WeatherCoreData>(entityName: "WeatherCoreData")
+    }
+
+    @NSManaged public var id: UUID?
+    @NSManaged public var text: String?
+    @NSManaged public var date: Date?
+    @NSManaged public var temperature: String?
+    @NSManaged public var weatherCondition: String?
+    @NSManaged public var weatherIcon: String?
+    @NSManaged public var location: String?
+
+}
+
+extension WeatherCoreData : Identifiable {
+
+}
