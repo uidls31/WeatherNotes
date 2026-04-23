aed29532a4e5f68e00da2a1f67f741a1bbb98f80
uidls31 <lagutin7234@gmail.com>
2026-04-23 13:08:38 +0200

    Initial Commit

---
+++ WeatherNotes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherNotes
+//
+//  Created by Александр Лагутин on 23/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherNotes/WeatherNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNotesApp.swift
+//  WeatherNotes
+//
+//  Created by Александр Лагутин on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
